92bee30cf242d96bb85ff3ddb0c9746f080b7fd2
EnriqueMTzEkO <enriquemartinez93893@gmail.com>
2024-09-24 12:42:25 -0600

    asincronia

--- src/app/page/action-sheet/action-sheet.page.ts
+++ src/app/page/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./action-sheet.page.scss'],
 })
 export class ActionSheetPage implements OnInit {
-
-  constructor() { }
+  [x: string]: any;
+pageTitle: string = '';
+  constructor(public actionSheetController:ActionSheetController ) { }
 
   ngOnInit() {
+  }
+
+  onClick(){
+this.presentActionSheet();
+  }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Actions',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
   }
 
 }
--- src/app/page/alert/alert.page.ts
+++ src/app/page/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCTRL:AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert(){
+    const alert = await this.alertCTRL.create({
+      header:'Alert',
+      subHeader:'subtitle',
+      message: 'This is an alert message.',
+      buttons: ['Ok']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
 }
